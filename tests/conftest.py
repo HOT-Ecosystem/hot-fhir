d29eda3f7aab069b9c7f36a60fda5c2b00fa375b
jiaola <djiao@jhu.edu>
2020-10-01 09:15:45 -0400

    rewrite test for neo4j with ph2neo graph

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,10 @@
 import pytest
 from fhirclient.models.codesystem import CodeSystem
 from flask import Flask
-from flask_on_fhir import FHIR, DataEngine
 import configparser
-from hot_fhir.data.neo4j import Neo4jModels, Neo4jDataEngine
 import requests
 from requests.exceptions import ConnectionError
+from py2neo import Graph
 
 
 def is_responsive(url):
@@ -23,29 +22,15 @@ def app():
     return app
 
 
-@pytest.fixture(scope="session")
-def neo4j_server(docker_ip, docker_services):
-    """ Ensure neo4j is up and responsive"""
-    port = docker_services.port_for("fhir-neo4j", 7474)
+@pytest.fixture(scope='session')
+def neo4j_graph(docker_ip, docker_services):
+    port = docker_services.port_for('fhir-neo4j', 7474)
     url = f'http://{docker_ip}:{port}'
     docker_services.wait_until_responsive(
-        timeout=30.0, pause=0.1, check=lambda: is_responsive(url)
+        timeout=30.0, pause=0.1, check=lambda : is_responsive(url)
     )
     bolt_port = docker_services.port_for('fhir-neo4j', 7687)
     bolt_url = f'bolt://{docker_ip}:{bolt_port}'
-    neo4j = Neo4jModels(bolt_url, 'neo4j', 'password')
-    yield neo4j
-    neo4j.close()
+    graph = Graph(bolt_url, auth=('neo4j', 'password'))
+    yield graph
 
-
-@pytest.fixture(scope="session")
-def data_engine(neo4j_server) -> Neo4jDataEngine:
-    engine = Neo4jDataEngine(neo4j_server)
-    yield engine
-    engine.close()
-
-
-@pytest.fixture
-def fhir(app, data_engine):
-    fhir = FHIR(app, data_engine)
-    return fhir
