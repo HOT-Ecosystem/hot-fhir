29ae936e387abf80530c2a143d7b125a5c66357c
jiaola <djiao@jhu.edu>
2020-10-02 08:17:38 -0400

    clean up tests

--- tests/hot_fhir/db/neo4j/test_db_session.py
+++
@@ -1,6 +0,0 @@
-from hot_fhir.db.neo4j.db_session import db_auth
-
-
-def test_db_auth():
-    graph = db_auth()
-    assert graph is not None
--- tests/hot_fhir/test_metadata.py
+++
@@ -1,4 +0,0 @@
-def test_metadata(client, fhir):
-    res = client.get('/metadata')
-    assert res.status_code == 200
-    assert res.json['resourceType'] == 'CapabilityStatement'
