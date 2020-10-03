a684ab59fdf1ca99518f14ac42d0dc9c113f411a
jiaola <djiao@jhu.edu>
2020-10-03 00:54:59 -0400

    remove flask-on-fhir

--- hot_fhir/app.py
+++ hot_fhir/app.py
@@ -1,14 +1,6 @@
 from flask import Flask
-from flask_on_fhir import FHIR
-from flask_on_fhir.restful_resources import NamingSystemResource
-from hot_fhir.data.neo4j import Neo4jDataEngine
-from importer.utils import neo4j_models
-
-engine = Neo4jDataEngine(neo4j_models())
 
 app = Flask(__name__)
-fhir = FHIR(app=app, data_engine=engine)
-fhir.add_fhir_resource(NamingSystemResource)
 
 
 @app.route('/')
